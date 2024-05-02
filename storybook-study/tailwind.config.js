a67ba4c243730f1f62eda7c617d8b8d15c17b357
teawon <xodnjsvkvk9@gmail.com>
2024-05-02 22:21:13 +0900

    chore: tailwind 테마 설정

--- storybook-study/tailwind.config.js
+++ storybook-study/tailwind.config.js
@@ -3,6 +3,52 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // 이걸 넣어야 React에서 작성한 스타일이 반영된다고 함
   theme: {
     extend: {},
+    fontSize: {
+      // 자주 쓰이는 폰트를 정리하면 편함 (24px의 경우 bold가 자주들어간다면 한꺼번에 묶는 것)
+      xs: [
+        "12px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "21px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      xl: ["20px", "30px"],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+    },
+    colors: {
+      primary: " #1d2745",
+      secondary: " #1de5d4",
+      tertiary: " #f52c50",
+      white: " #ffffff",
+      mono100: " #f1f1f1",
+      mono200: " #bebebe",
+      mono300: " #d6d7d9",
+      error: " #d01e1e",
+    },
   },
   plugins: [],
 };
