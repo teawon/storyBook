b27b4e5586e441dcebea5d4bba6fcccf6e13e5a5
teawon <xodnjsvkvk9@gmail.com>
2024-05-05 22:57:33 +0900

    feat: Button 컴포넌트 (theme)

--- storybook-study/tailwind.config.js
+++ storybook-study/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // 이걸 넣어야 React에서 작성한 스타일이 반영된다고 함
   theme: {
     extend: {},
+    borderRadius: {
+      "primary-button": "5px",
+    },
     fontFamily: {
       body: ["Noto Sans KR"],
     },
